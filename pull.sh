895982a203794892118268b4aea93f56bd61b4f2
Romain Lecat <romain@lecat.io>
2021-05-04 15:45:01 +0200

    Update kexts and resources

--- pull.sh
+++ pull.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sudo mount -t msdos -o ro /dev/disk0s1 ./tmp-efi
-rsync -rav --delete --exclude ".empty" --exclude "._*"  ./tmp-efi/EFI/* .
+#sudo mount -t msdos -o ro /dev/disk0s1 ./tmp-efi
+rsync -rav --delete --exclude ".empty" --exclude "._*"  /Volumes/EFI/EFI/* .
 sudo diskutil unmount /dev/disk0s1
